118709c21b5a2f585af12f2d77cc6dee9e0ce579
Snir Turgeman <snir.tur@gmail.com>
2026-09-16 09:02:59 +0300

    Drop the committed target-linux build directory (#97)
    #82 checked in a 159 MB target-linux/ cargo output directory. The package
    step ships git archive HEAD, so every v0.5.0 tarball grew from 2 MB to
    57 MB. Untrack it and ignore target-* alongside target.

--- target-linux/release/deps/agenmux_notifier-8b4561c2b0ce9cdb.d
+++
@@ -1,6 +0,0 @@
-/src/target-linux/release/deps/agenmux_notifier-8b4561c2b0ce9cdb.d: src/bin/agenmux-notifier.rs src/bin/agenmux-notifier/broker.rs
-
-/src/target-linux/release/deps/agenmux_notifier-8b4561c2b0ce9cdb: src/bin/agenmux-notifier.rs src/bin/agenmux-notifier/broker.rs
-
-src/bin/agenmux-notifier.rs:
-src/bin/agenmux-notifier/broker.rs:
--- target-linux/release/deps/serde_spanned-e489e9af1c5765a9.d
+++
@@ -1,8 +0,0 @@
-/src/target-linux/release/deps/serde_spanned-e489e9af1c5765a9.d: /usr/local/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs /usr/local/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs
-
-/src/target-linux/release/deps/libserde_spanned-e489e9af1c5765a9.rlib: /usr/local/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs /usr/local/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs
-
-/src/target-linux/release/deps/libserde_spanned-e489e9af1c5765a9.rmeta: /usr/local/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs /usr/local/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs
-
-/usr/local/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/lib.rs:
-/usr/local/cargo/registry/src/index.crates.io-1949cf8c6b5b557f/serde_spanned-0.6.9/src/spanned.rs:
